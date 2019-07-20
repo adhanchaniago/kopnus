13eef4ff407446c753789318b10a7285216db44e
ivan471 <>
2019-07-20 19:57:39 +0800

    Update 1.6.1
    Perbaikan Laporan Pdf

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -1,9 +1,11 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 class laporan extends CI_Controller
 {
-	function __construct(){
+	public function __construct() {
 		parent::__construct();
 		$this->load->library('pdf');
+		$this->load->model('model_user');
 	}
 	function index($id){
 		$pdf = new FPDF('p','mm','A4');
@@ -19,21 +21,17 @@ class laporan extends CI_Controller
 		// Memberikan space kebawah agar tidak terlalu rapat
 		$pdf->Ln(4);
 		$pdf->SetFont('Arial','',10);
-		$laporan = $this->db->query("SELECT * from tb_user where norek= '".$id."' ")->result();
-		foreach ($laporan as $row){
-			$upper = strtoupper($row->nama);
-			//$pdf->Cell(20,10,'Norek',0,0);
-			//$pdf->Cell(20,10,$row->norek,0,1);
-			//$pdf->Cell(20,10,'Nama',0,0);
-			//$pdf->Cell(20,10,$d,0,1);
-			//$pdf->Cell(20,10,'Alamat',0,0);
-			//$pdf->Cell(20,10,$row->alamat,0,1);
-			$alamat = $row->alamat;
-			$pdf->SetLeftMargin(30);
-			$pdf->SetRightMargin(30);
-			$pdf->Cell(200,10,'Pada hari ini, tanggal '.date('d M Y').', telah terjadi perjanjian kredit antara :',0,1);
-			$pdf->cell(200,10,'1. '.$upper.' bertempat tinggal di '.$alamat,0,1);
-		}
+		$data = $this->db->query("SELECT * FROM tb_user WHERE norek = '" .$id. "'");
+		$ssd = $data->row();
+		$upper = strtoupper($ssd->nama);
+		$alamat = $ssd->alamat;
+		$pdf->SetLeftMargin(28);
+		$pdf->SetRightMargin(28);
+		$pdf->cell(200,10,'Yang bertanda tangan dibawah ini :',0,1);
+		$pdf->cell(5,6,'I.	',0,0);
+		$pdf->multicell(0,6,'ANDI BASO SARIPUDIN dalam hal ini bertindak dalam kedudukan selaku Kepala Bisnis KSP Nusantara dari Koperasi Nusantara, Kantor Cabang Utama Makassar oleh karena itu bertindak untuk dan atas nama Koperasi Nusantara, Berkedudukan di Kantor Wilayah.',0,'J',false);
+		$pdf->cell(5,6,'II.	',0,0);
+		$pdf->multicell(0,6,$upper.', swasta, bertempat tinggal di '.$alamat.' dalam hal ini bertindak untuk diri sendiri, selanjutnya disebut DEBITOR.',0,'J',false);
 		$pdf->Output();
 	}
 }
